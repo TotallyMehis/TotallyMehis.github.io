ba8f2a610a09584ffaf407035ff25acb13ee7431
Miikka Ylätalo <totally.mehis@gmail.com>
2023-12-01 17:42:25 +0200

    Add sitemap.xml & robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -18,6 +18,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ 'misc/CNAME': 'CNAME' })
   eleventyConfig.addPassthroughCopy({ 'misc/.nojekyll': '.nojekyll' })
+  eleventyConfig.addPassthroughCopy({ 'misc/robots.txt': 'robots.txt' })
 
   eleventyConfig.addPlugin(syntaxHighlight)
 
